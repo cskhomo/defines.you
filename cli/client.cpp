7e1a3cfe76fc703fd51774bca4222bc5b1d2ec27
Chrisser <164577349+cskhomo@users.noreply.github.com>
2026-04-11 23:58:02 +0200

    implement argh! basic

--- cli/client.cpp
+++ cli/client.cpp
@@ -8,31 +8,35 @@ int main(int argc, char* argv[]){
   
   parser cmdl(argv);
   
-  if (not (argc-1) ){
-    cout << "print help" << endl;
+  bool help = cmdl[{"h", "help"}];
+  bool redefine = cmdl[{"r", "redefine"}];
+  
+  if (help){
+    cout << "print help (general)" << endl;
+    return 1;
+  }
+  
+  if (cmdl[1].empty()){
+    cout << "print help (less)" << endl;
     return 1;
   }
   
   string term;
   
-  if (cmdl[{"r", "redefine"}]){
-    
-    try {
-      term = argv[2];
-    }
-    
-    catch (exception e){
-      cout << "error";
-      return 1;
-    }
+  if (redefine){
+    term = cmdl[1] + " -r";
   }
   
   if (not cmdl[2].empty()) {
-    cout << "print help" << endl;
+    cout << "print help (more)" << endl;
     return 1;
   }
   
-  term = cmdl[1];
+  if (term.empty()) {
+    term = cmdl[1];
+  }
+  
   cout << "send request: " << term << endl;
+  
   return 0;
 }
