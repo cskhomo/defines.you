f9f4d751783f0ea058fb15ff7ff2cfd699f82138
Chrisser <164577349+cskhomo@users.noreply.github.com>
2026-05-26 19:07:45 +0200

    clean

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-rm -f calculator.so
-g++ -O3 -Wall -shared -std=c++11 -fPIC $(python3 -m pybind11 --includes) calculator.cpp bindings.cpp -o calculator.so
+rm -f test/calculator.so
+g++ -O3 -Wall -shared -std=c++11 -fPIC $(python3 -m pybind11 --includes) main/calculator.cpp main/bindings.cpp -o test/calculator.so
